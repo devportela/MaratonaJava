2d7ec5a9fff3b0aab17bb289aa265e147b9ceac9
gustavo portela <portelagustavo947@gmail.com>
2025-07-13 12:23:53 -0300

    metodos getter and setters e private

--- src/POOclasses/metodos/domain/Funcionario.java
+++ src/POOclasses/metodos/domain/Funcionario.java
@@ -1,18 +1,61 @@
 package POOclasses.metodos.domain;
-
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double salario;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
-    public void imprimeDados() {
+    public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.idade);
-        System.out.println(this.salario);
-        System.out.println("------------------------");
+        if (salarios == null) {
+            return;
+        }
+        for (double salario : salarios) {
+            System.out.print(salario + " ");
+        }
 
+        imprimeMediaSalario();
     }
 
+    public void imprimeMediaSalario() {
+        if (salarios == null) {
+            return;
+        }
 
+        for (double salario : salarios) {
+            media += salario;
+        }
+        media /= salarios.length;
 
+        System.out.println("\nMédia salarial " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+            return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
+    }
 }
--- src/POOclasses/metodos/test/FuncionarioTeste01.java
+++ src/POOclasses/metodos/test/FuncionarioTeste01.java
@@ -1,42 +1,14 @@
 package POOclasses.metodos.test;
 
 import POOclasses.metodos.domain.Funcionario;
-import POOclasses.metodos.domain.MediaSalarial;
 
 public class FuncionarioTeste01 {
     public static void main(String[] args) {
-        MediaSalarial media = new MediaSalarial();
-        Funcionario funcionario01 = new Funcionario();
-        Funcionario funcionario02 = new Funcionario();
-        Funcionario funcionario03 = new Funcionario();
-
-
-        funcionario01.nome = "Gustavo";
-        funcionario01.idade = 16;
-        funcionario01.salario = 5500;
-
-        funcionario02.nome = "José";
-        funcionario02.idade = 43;
-        funcionario02.salario = 3500;
-
-        funcionario03.nome = "Junior ";
-        funcionario03.idade = 28;
-        funcionario03.salario = 4500;
-
-        funcionario01.imprimeDados();
-        funcionario02.imprimeDados();
-        funcionario03.imprimeDados();
-
-        media.mediaSalarioFuncionario(funcionario01.salario,funcionario02.salario, funcionario03.salario);
-
-
-
-
-
-
-
-
-
-
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Sanji");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
+        funcionario.imprime();
+        System.out.println("Média " + funcionario.getMedia());
     }
 }
