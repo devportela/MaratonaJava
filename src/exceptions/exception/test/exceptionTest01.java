8713f4c2ed38bff87d1a8a7bf81209232f58fcf0
gustavo portela <portelagustavo947@gmail.com>
2025-08-16 11:22:32 -0300

    insert arquivo

--- src/exceptions/exception/test/exceptionTest01.java
+++ src/exceptions/exception/test/exceptionTest01.java
@@ -1,7 +1,27 @@
 package exceptions.exception.test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class exceptionTest01 {
     public static void main(String[] args) {
+        criarNovoArquivo();
 
     }
+
+    private static void criarNovoArquivo() {
+
+
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("arquivo criado com sucesso" + isCriado);
+        } catch (IOException e) {
+            //NUNCA DEIXE ESSE BLOCO EM BRANCO
+            //evitar por logica de negocio no catch
+            e.printStackTrace();
+        }
+
+    }
+
 }
